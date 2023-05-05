d96cd06e65933692beb78481d99b8eb045a93948
Jorge Carrillo <luisjorgecarrilloq@gmail.com>
2023-05-05 16:02:14 -0600

    CSC SelectedValueChanged left

---
+++ UsersApp/Models/CSCUser.cs
@@ -0,0 +1,20 @@
+﻿namespace UsersApp.Models
+{
+    public class CSCUser
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Position { get; set; }
+        public String Department { get; set; }
+        public int GroupId { get; set; }
+    }
+
+    public class CSCGroups
+    {
+        public Int32 Id { get; set; }
+        public String Name { get; set; }
+
+
+
+    }
+}
