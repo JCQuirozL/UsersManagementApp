d975ebba7754176d96db776e75773190c717454b
Jorge Carrillo <luisjorgecarrilloq@gmail.com>
2023-05-08 13:02:43 -0600

    Desembolso Electrónico terminado

---
+++ UsersApp/Models/DEUser.cs
@@ -0,0 +1,31 @@
+﻿namespace UsersApp.Models
+{
+    public class DEUser
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int DepartmentId { get; set; }
+
+        public int RolId { get; set; }
+        
+        public Boolean EsJefeInmediato { get; set; }
+
+        public DateTime InfoDate { get; set; }
+
+        public String CreadoPor { get; set; }
+    }
+
+    public class DEDepartments
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+    }
+
+    public class DERol
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+    }
+}
---
+++ UsersApp/Models/SUCUser.cs
@@ -0,0 +1,20 @@
+﻿namespace UsersApp.Models
+{
+    public class SUCUser
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+
+        public String UserName { get; set; }
+
+        public int DepartmentId { get; set; }
+
+        public DateTime InfoDate { get; set; }
+    }
+
+    public class SUCDepartments
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+    }
+}
